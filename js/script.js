0af1e452ce6cca269153dd6e1aa816fa6b6161a7
Kwatmi Haruna Paul <kwatmiharunapaul@gmail.com>
2020-06-21 15:09:24 +0100

    calculate sum

--- js/script.js
+++ js/script.js
@@ -88,6 +88,36 @@ return false;
 });
 
 
+// adding check box value  ammount
+
+
+// $(document).ready(function() {
+//       function updateSum() {
+//         var total = 0;
+//         $(".sum:checked").each(function(i, n) {total += parseInt($(n).val());})
+//         $("#total").val(total);
+//       }
+//       // run the update on every checkbox change and on startup
+//       $("input.sum").change(updateSum);
+//       updateSum();
+//   })
+
+
+
+  window.onload=function(){
+      var inputs = document.getElementsByClassName('sum'),
+          total  = document.getElementById('payment-total');
+      
+       for (var i=0; i < inputs.length; i++) {
+          inputs[i].onchange = function() {
+              var add = this.value * (this.checked ? 1 : -1);
+              total.innerHTML = parseFloat(total.innerHTML) + add
+              var new_total = parseFloat(document.getElementById('input').value);
+            console.log( new_total);
+              document.getElementById('input').value= new_total  + add
+          }
+        }
+      }
 
 // slider
 
